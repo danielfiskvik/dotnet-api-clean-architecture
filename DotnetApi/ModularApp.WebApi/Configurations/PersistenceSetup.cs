84d2a85464b78337b9883609e82cb6e40b96acc7
Daniel Fiskvik <daniel-fiskvik@hotmail.com>
2023-07-03 19:34:39 +0200

    [KanjiRepo] DbContext to set context properly

--- DotnetApi/ModularApp.WebApi/Configurations/PersistenceSetup.cs
+++ DotnetApi/ModularApp.WebApi/Configurations/PersistenceSetup.cs
@@ -6,11 +6,11 @@ public static class PersistenceSetup
 {
     public static async Task InitializePersistenceModuleAsync(this WebApplication app, IConfiguration configuration)
     {
-        // using var scope = app.Services.CreateScope();
-        // var initializer = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
-        //
-        // // Initialise and seed database
-        // await initializer.InitialiseAsync(configuration);
-        // await initializer.SeedAsync();
+        using var scope = app.Services.CreateScope();
+        var initializer = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
+        
+        // Initialise and seed database
+        await initializer.InitialiseAsync(configuration);
+        await initializer.SeedAsync();
     }
 }
